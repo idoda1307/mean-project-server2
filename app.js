4af8090c7fce21399472e4a5cf118f5c10be3db7
idoda1307 <idoda1307@gmail.com>
2018-07-31 10:35:19 +0300

    commit

--- app.js
+++ app.js
@@ -22,18 +22,18 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));  
 app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization, Unauthorized"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, PATCH, DELETE, OPTIONS"
-//     );
-//     next();
-// });
+ app.use((req, res, next) => {
+     res.setHeader("Access-Control-Allow-Origin", "*");
+     res.setHeader(
+         "Access-Control-Allow-Headers",
+         "Origin, X-Requested-With, Content-Type, Accept, Authorization, Unauthorized"
+     );
+     res.setHeader(
+         "Access-Control-Allow-Methods",
+         "GET, POST, PATCH, DELETE, OPTIONS"
+     );
+     next();
+ });
 
 app.use("/api/user", userRoutes);
 app.use("/api/event",eventRoutes);
