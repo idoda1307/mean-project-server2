921eb93bd33ec01c0bc585ee2c1c21fa0b739334
idoda1307 <idoda1307@gmail.com>
2018-08-06 19:00:42 +0300

    commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());                                    
 app.use(bodyParser.json({ type: 'application/json'}));  
 app.use(cors());
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('images')));
 
  app.use((req, res, next) => {
      res.setHeader("Access-Control-Allow-Origin", "*");
